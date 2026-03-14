a2d72a9056792d9b32e8b2840d94833790aec53c
Aces77778 <acesachilles.najarro@gmail.com>
2026-03-14 18:06:19 +0800

    adjusted the book into the main UI
    -added the new assets, scenes, and script into the game
    -adjusted the book into main UI (inspection_2d), made it functional
    -all changes are about the book, no changes were made with other assets, scenes, and scripts

--- Front Desk/script/Handbook/handbook_opened.gd
+++ Front Desk/script/Handbook/handbook_opened.gd
@@ -9,31 +9,23 @@ var page_frames = {
 	"contents": 0, #The main contents page frame
 	"basic_rules": 1, #The basic rules page frame
 	"basic_rules_hover": 7, #The hover state for basic rules
-	"employees": 2, #The employees page frame
-	"employees_hover": 8, #The hover state for employees
-	"e_one": 2, #E-1 page frame
-	"e_one_hover": 9, #The hover state for E-1
-	"e_two": 3, #E-2 page frame
-	"e_two_hover": 10, #The hover state for E-2
-	"e_three": 4, #E-3 page frame
-	"e_three_hover": 11, #The hover state for E-3
-	"e_four": 5, #E-4 page frame
-	"e_four_hover": 12 #The hover state for E-4
+	"professor1": 3, #The employees page frame
+	"professor_hover": 8, #The hover state for professor
+	"canteen_staff": 5, #E-1 page frame
+	"canteen_staff_hover": 9, #The hover state for E-1
 }
 @onready var basic_rules_button: Button = %BasicRules
-@onready var employees_button: Button = %Employees
-@onready var e_one_button: Button = %"E-1"
-@onready var e_two_button: Button = %"E-2"
-@onready var e_three_button: Button = %"E-3"
-@onready var e_four_button: Button = %"E-4"
+@onready var professor_button: Button = %Professor
+@onready var canteen_staff_button: Button = %CanteenStaff
 @onready var bookmark_button: Button = %"Bookmark (Red)"
 
 #Variables for Handbook
-@onready var handbook_opened_node2D: Node2D = %HandbookOpened
+@onready var handbook_icon: Node2D = %HandbookIcon
+@onready var handbook: Node2D = %Handbook
 
 #Variables for Flipping the Book
 var flip = false
-@onready var handbook_opened_sprite: AnimatedSprite2D = %HandbookOpen
+@onready var handbook_opened_sprite = %HandbookOpen
 var current_frame = 0
 var total_frames = 7
 var frame_duration = 0.1
@@ -46,21 +38,12 @@ func _ready() -> void:
 	basic_rules_button.mouse_entered.connect(_on_basic_rules_hover)
 	basic_rules_button.mouse_exited.connect(_on_basic_rules_unhover)
 	basic_rules_button.pressed.connect(_on_basic_rules_clicked)
-	employees_button.mouse_entered.connect(_on_employees_hover)
-	employees_button.mouse_exited.connect(_on_employees_unhover)
-	employees_button.pressed.connect(_on_employees_clicked)
-	e_one_button.mouse_entered.connect(_on_e_one_hover)
-	e_one_button.mouse_exited.connect(_on_e_one_unhover)
-	e_one_button.pressed.connect(_on_e_one_clicked)
-	e_two_button.mouse_entered.connect(_on_e_two_hover)
-	e_two_button.mouse_exited.connect(_on_e_two_unhover)
-	e_two_button.pressed.connect(_on_e_two_clicked)
-	e_three_button.mouse_entered.connect(_on_e_three_hover)
-	e_three_button.mouse_exited.connect(_on_e_three_unhover)
-	e_three_button.pressed.connect(_on_e_three_clicked)
-	e_four_button.mouse_entered.connect(_on_e_four_hover)
-	e_four_button.mouse_exited.connect(_on_e_four_unhover)
-	e_four_button.pressed.connect(_on_e_four_clicked)
+	professor_button.mouse_entered.connect(_on_professor_hover)
+	professor_button.mouse_exited.connect(_on_professor_unhover)
+	professor_button.pressed.connect(_on_professor_clicked)
+	canteen_staff_button.mouse_entered.connect(_on_canteen_staff_hover)
+	canteen_staff_button.mouse_exited.connect(_on_canteen_staff_unhover)
+	canteen_staff_button.pressed.connect(_on_canteen_staff_clicked)
 	bookmark_button.pressed.connect(_on_bookmark_clicked)
 
 #Functions for Flipping the Book
@@ -96,38 +79,21 @@ func update_button_visibility():
 		basic_rules_button.disabled = false
 		basic_rules_button.mouse_filter = Control.MOUSE_FILTER_STOP
 		
-		employees_button.disabled = false
-		employees_button.mouse_filter = Control.MOUSE_FILTER_STOP
+		professor_button.disabled = false
+		professor_button.mouse_filter = Control.MOUSE_FILTER_STOP
 		
-		e_one_button.disabled = false
-		e_one_button.mouse_filter = Control.MOUSE_FILTER_STOP
-		
-		e_two_button.disabled = false
-		e_two_button.mouse_filter = Control.MOUSE_FILTER_STOP
-		
-		e_three_button.disabled = false
-		e_three_button.mouse_filter = Control.MOUSE_FILTER_STOP
-		
-		e_four_button.disabled = false
-		e_four_button.mouse_filter = Control.MOUSE_FILTER_STOP
+		canteen_staff_button.disabled = false
+		canteen_staff_button.mouse_filter = Control.MOUSE_FILTER_STOP
+
 	else:
 		basic_rules_button.disabled = true
 		basic_rules_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		
-		employees_button.disabled = true
-		employees_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		professor_button.disabled = true
+		professor_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		
-		e_one_button.disabled = true
-		e_one_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		
-		e_two_button.disabled = true
-		e_two_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		
-		e_three_button.disabled = true
-		e_three_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		
-		e_four_button.disabled = true
-		e_four_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		canteen_staff_button.disabled = true
+		canteen_staff_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 #Functions for Drag and Drop
 func _process(_delta):
@@ -150,9 +116,14 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 
 #For small handbook pop-up function
 func _on_button_pressed() -> void:
-	handbook_opened_node2D.visible = !handbook_opened_node2D.visible
-	if handbook_opened_node2D.visible:
-		handbook_opened_node2D.position = Vector2(800, 300) #Places book consistently on the same spot
+	handbook.visible = !handbook.visible
+	if handbook.visible:
+		handbook_opened_sprite.global_position = Vector2(800, 350) #Places book consistently on the same spot
+		current_frame = page_frames["contents"]
+		handbook_opened_sprite.frame = current_frame
+		basic_rules_button.disabled = false
+		professor_button.disabled = false
+		canteen_staff_button.disabled = false
 
 #For First page button functions
 func _on_basic_rules_hover():
@@ -171,72 +142,46 @@ func _on_basic_rules_clicked():
 	handbook_opened_sprite.frame = current_frame
 	#For Button node to disappear to avoid interference with other pages
 	basic_rules_button.disabled = true
+	professor_button.disabled = true
+	canteen_staff_button.disabled = true
 
-func _on_employees_hover():
+func _on_professor_hover():
 	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["employees_hover"]
+		handbook_opened_sprite.frame = page_frames["professor_hover"]
 
-func _on_employees_unhover():
-	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["contents"]
-
-func _on_employees_clicked():
-	current_frame = page_frames["employees"]
-	handbook_opened_sprite.frame = current_frame
-	employees_button.disabled = true
-
-func _on_e_one_hover():
-	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["e_one_hover"]
-
-func _on_e_one_unhover():
+func _on_professor_unhover():
 	if current_frame == page_frames["contents"]:
 		handbook_opened_sprite.frame = page_frames["contents"]
 
-func _on_e_one_clicked():
-	current_frame = page_frames["e_one"]
+func _on_professor_clicked():
+	current_frame = page_frames["professor1"]
 	handbook_opened_sprite.frame = current_frame
-	e_one_button.disabled = true
+	basic_rules_button.disabled = true
+	professor_button.disabled = true
+	canteen_staff_button.disabled = true
 
-func _on_e_two_hover():
+
+func _on_canteen_staff_hover():
 	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["e_two_hover"]
+		handbook_opened_sprite.frame = page_frames["canteen_staff_hover"]
 
-func _on_e_two_unhover():
+func _on_canteen_staff_unhover():
 	if current_frame == page_frames["contents"]:
 		handbook_opened_sprite.frame = page_frames["contents"]
 
-func _on_e_two_clicked():
-	current_frame = page_frames["e_two"]
+func _on_canteen_staff_clicked():
+	current_frame = page_frames["canteen_staff"]
 	handbook_opened_sprite.frame = current_frame
-	e_two_button.disabled = true
-
-func _on_e_three_hover():
-	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["e_three_hover"]
-
-func _on_e_three_unhover():
-	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["contents"]
-
-func _on_e_three_clicked():
-	current_frame = page_frames["e_three"]
-	handbook_opened_sprite.frame = current_frame
-	e_three_button.disabled = true
-
-func _on_e_four_hover():
-	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["e_four_hover"]
-
-func _on_e_four_unhover():
-	if current_frame == page_frames["contents"]:
-		handbook_opened_sprite.frame = page_frames["contents"]
-
-func _on_e_four_clicked():
-	current_frame = page_frames["e_four"]
-	handbook_opened_sprite.frame = current_frame
-	e_four_button.disabled = true
+	basic_rules_button.disabled = true
+	professor_button.disabled = true
+	canteen_staff_button.disabled = true
 
 func _on_bookmark_clicked():
 	current_frame = page_frames["contents"]
 	handbook_opened_sprite.frame = current_frame
+	basic_rules_button.disabled = false
+	basic_rules_button.mouse_filter = Control.MOUSE_FILTER_STOP #When bookmark is pressed, buttons will work properly again
+	professor_button.disabled = false
+	professor_button.mouse_filter = Control.MOUSE_FILTER_STOP
+	canteen_staff_button.disabled = false
+	canteen_staff_button.mouse_filter = Control.MOUSE_FILTER_STOP
